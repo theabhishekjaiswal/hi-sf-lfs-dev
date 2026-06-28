996a4caf49c5b90bdabc10b1df1a637818ec5e4d
Abhishek Jaiswal <abhivihsaljaiswal420@gmail.com>
2026-06-28 17:06:02 +0530

    fix/6.4

--- content.js
+++ content.js
@@ -708,11 +708,19 @@
         } else {
           destination = 'setup/ui/customsettings.jsp';
         }
+      } else if (obj.type === 'Class') {
+        // Direct Apex Class Setup page in Classic (safely bypasses Lightning redirections)
+        destination = `setup/build/apxClass.apexp?id=${obj.setupId}`;
+      } else if (obj.type === 'Trigger') {
+        // Direct Apex Trigger Setup page in Classic
+        destination = `setup/build/apxTrigger.apexp?id=${obj.setupId}`;
+      } else if (obj.type === 'Page') {
+        // Direct Visualforce Page Setup page in Classic
+        destination = `setup/build/apxPage.apexp?id=${obj.setupId}`;
       } else if (obj.type === 'Label') {
         // Custom Label detail Setup ID
         destination = obj.setupId;
       } else {
-        // Apex Class, Apex Trigger, Visualforce Page setup detail page
         destination = obj.setupId;
       }
     } else {
